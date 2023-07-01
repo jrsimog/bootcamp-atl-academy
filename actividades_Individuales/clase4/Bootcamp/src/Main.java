da6ed1847076554f0973aa99e01a4723c50e6e9b
José <jose@recemed.cl>
2023-07-01 05:03:06 -0400

    test clase 4

---
+++ actividades_Individuales/clase4/Bootcamp/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // var de tipo string
+        String variableSaludo = "Hola, soy ";
+        String miNombre = "José";
+        // var de tipo entero
+        Integer nroEquipo = 2;
+        String texto = variableSaludo + miNombre +
+                ", estoy en el equipo #"+nroEquipo;
+        System.out.println(texto);
+    }
+}
