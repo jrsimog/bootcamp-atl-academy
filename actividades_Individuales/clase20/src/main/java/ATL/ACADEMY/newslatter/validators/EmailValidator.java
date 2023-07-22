04ff42fa289ba688d7bac5d42003098f43bf14f0
José <jose@recemed.cl>
2023-07-22 04:57:20 -0400

    agregar expresion regular para validar email

--- actividades_Individuales/clase20/src/main/java/ATL/ACADEMY/newslatter/validators/EmailValidator.java
+++ actividades_Individuales/clase20/src/main/java/ATL/ACADEMY/newslatter/validators/EmailValidator.java
@@ -3,23 +3,14 @@ package ATL.ACADEMY.newslatter.validators;
 public class EmailValidator {
 
     public boolean esValido(String email){
-        if(!email.contains("@")){
-            return false;
-        }
-        if(!email.contains(".")){
-            return false;
-        }
-        if(email.contains("!")){
-            return false;
-        }
-
-        if(email.contains(" ")){
-            return false;
-        }
-
-        if(email.length() > 254){
-            return false;
-        }
-        return true;
+        // Expresión regular para validar un correo electrónico
+        // ^[a-zA-Z0-9._%+-]+ : Comienza con uno o más caracteres permitidos (letras, números, ., _, %, +, -)
+        // @ : Seguido de un carácter @
+        // [a-zA-Z0-9.-]+ : Seguido de uno o más caracteres permitidos para el dominio (letras, números, ., -)
+        // \\. : Seguido de un carácter punto literal
+        // [a-zA-Z]{2,6} : Seguido de entre 2 y 6 letras (para el TLD,.. es el acrónimo de Top-Level Domain, que se traduce como Dominio de Nivel Superior en español. Se refiere a la última parte de un nombre de dominio en Internet.)
+        // $ : Fin de línea
+        String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
+        return email.matches(regex);
     }
 }
